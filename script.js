74f13736b127375234c58124322ef5cff3b6c7a5
parthpd24 <parthpd24@gmail.com>
2016-05-21 21:16:13 -0400

    organization

---
+++ script.js
@@ -0,0 +1,9 @@
+function exportToCsv() {
+    var name = $("#name-input").val();
+    var place = $("#do-input").val()
+    var like =  $("#like-input").val()
+    var nolike =  $("#dislike-input").val()
+    console.log(name)
+}
+
+$("#b").click(exportToCsv);
