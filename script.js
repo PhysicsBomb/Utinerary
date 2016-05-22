8f896a96c3d11c8c4ef8937dcbdebb769911ce0b
parthpd24 <parthpd24@gmail.com>
2016-05-21 22:26:32 -0400

    Set out first stuffs

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+var name = 1;
+var dostuff = 1;
+var like = 1;
+var nolike = 1;
+
 function exportToCsv() {
-    var name = $("#name-input").val();
-    var place = $("#do-input").val()
-    var like =  $("#like-input").val()
-    var nolike =  $("#dislike-input").val()
-    console.log(name)
+    name = $("#name-input").val();
+    dostuff = $("#do-input").val();
+    like =  $("#like-input").val();
+    nolike =  $("#dislike-input").val();
+    console.log(name);
 }
 
 $("#b").click(exportToCsv);
+
+function formatgooglemapslink(place) {
+    
+}
+
+$.ajax({
+    
+});
